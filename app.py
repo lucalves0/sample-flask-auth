3daa43e4a2c1d2a55d24b02f9b25bcf51c193891
luca <114783933+lucalves0@users.noreply.github.com>
2024-04-29 21:33:45 -0300

    criação do banco de dados

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCREMY_DATABASE_URI'] = 'sqlite:///database.db' # caminho da conexão com o banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # caminho da conexão com o banco de dados
 
-db = SQLAlchemy(app)    # conexão com API 
+db.init_app(app)
 
+"""
+Comando para se usar na criação do banco e dentro do banco para realizar as operações
+
+- flask shell -> comando para entrar para realizar a criação do banco
+- db.create_all() -> cria o banco de dados
+- db.session -> cria uma sessão dentro do banco (cada usuario tem o seu próprio)
+- db.session.commit()-> pega tudo que foi realizado na sessão e executa o comando no banco da dados
+
+-- Banco Relacional -- 
+
+"""
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
     return "Hello World"
